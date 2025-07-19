3aa7c9c6279fb454260f14c0e26ed0881f734a01
Immaculate <geoffreyimmaculate@gmail.com>
2025-07-19 22:47:22 +0100

    Made corrections in the structure

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from .models import Book
 
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()  # <-- REQUIRED
+    return render(request, "relationship_app/list_books.html", {"books": books})  
+from django.views.generic.detail import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
