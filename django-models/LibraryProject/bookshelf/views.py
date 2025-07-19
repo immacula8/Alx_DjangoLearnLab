7953f54d7e4d97df9e9b75ea163d746ece5fed16
Immaculate <geoffreyimmaculate@gmail.com>
2025-07-20 00:03:24 +0100

    Made changes

--- django-models/LibraryProject/bookshelf/views.py
+++ django-models/LibraryProject/bookshelf/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book
+from relationship_app.models import Book
 
 def book_list(request):
     books = Book.objects.all()
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic import DetailView
+from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()  # <-- REQUIRED
@@ -10,5 +11,5 @@ from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"  # <== this exact string is required
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
