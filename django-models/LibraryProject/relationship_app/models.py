fba66286f727d5e78f9703baddc46d09d519b71a
Immaculate <geoffreyimmaculate@gmail.com>
2025-07-20 07:19:23 +0100

    Removed a space in the models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -26,3 +30,24 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(
+        max_length=20,
+        choices=[
+            ('Admin', 'Admin'),
+            ('Librarian', 'Librarian'),
+            ('Member', 'Member'),
+        ]
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+# Automatically create UserProfile on user creation
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
