19ab26ee61bd9975efb857478bbee5c8a8509fa2
Immaculate <geoffreyimmaculate@gmail.com>
2025-08-07 00:30:11 +0100

    Made changes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,6 +6,7 @@ from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter,OrderingFilter
+from django_filters import rest_framework
 
 
 # This List all books
