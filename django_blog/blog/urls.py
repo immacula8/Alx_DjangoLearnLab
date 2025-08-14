b867d76e308a04485ddb7948557e4275390a9586
Immaculate <geoffreyimmaculate@gmail.com>
2025-08-14 23:00:07 +0100

    Changed posts to post for the checker to pass it

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,18 +13,17 @@ urlpatterns = [
     # Registration
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
-
-    # Profile
     path('profile/', views.profile, name='profile'),
 
     # Login & Logout (using Django's built-in auth views)
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-    path('posts/', views.posts, name='posts'),
-    path('posts/', PostListView.as_view(), name='posts'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    path('post/', PostListView.as_view(), name='posts'),
+
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
 
