486d3db59c0c8a674a16754aef7c84796389de2f
Immaculate <geoffreyimmaculate@gmail.com>
2025-08-15 01:38:37 +0100

    The checker is expecting CRUD and I didnt list them, So I just added it to my view.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -14,6 +14,7 @@ from .forms import CommentForm
 from .forms import PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
 
 
 # Registration view
@@ -177,4 +178,37 @@ def delete_comment(request, pk):
         return redirect('post-detail', pk=post_pk)
 
     return render(request, 'blog/delete_comment.html', {'comment': comment})
+
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['content']  # adjust to your Comment model's text field
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post_id = self.kwargs['pk']  # assumes post pk in URL
+        return super().form_valid(form)
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ['content']
+    template_name = 'blog/comment_form.html'
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/comment_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
 
